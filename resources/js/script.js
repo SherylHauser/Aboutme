dab0ad81151c649ffcab7813c0fa39fbb8e63931
Andrin <81522951+AndBlx@users.noreply.github.com>
2023-06-23 14:22:55 +0200

    added links and download

--- resources/js/script.js
+++ resources/js/script.js
@@ -6,28 +6,34 @@ $(document).ready(function () {
         if (target.length) {
             $('html, body').animate({
                 scrollTop: target.offset().top
-            }, 1000);
+            }, 2000);
         }
     });
 });
 
 // ScrollReveal animations
 ScrollReveal().reveal('.hero-content', {
-    delay: 200,
+    delay: 300,
     distance: '50px',
     origin: 'bottom',
     easing: 'ease-in-out'
 });
 ScrollReveal().reveal('.section', {
-    delay: 200,
+    delay: 300,
     distance: '50px',
     origin: 'bottom',
     easing: 'ease-in-out'
 });
-ScrollReveal().reveal('.card', {
-    delay: 200,
+ScrollReveal().reveal('.projectsection', {
+    delay: 500,
     distance: '50px',
-    origin: 'bottom',
+    origin: 'left',
+    easing: 'ease-in-out'
+});
+ScrollReveal().reveal('.skill', {
+    delay: 250,
+    distance: '50px',
+    origin: 'right',
     easing: 'ease-in-out'
 });
 
